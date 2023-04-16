7e08f8eacf72636b47e2d43fc6a48bcbf492e452
adward <du-nguyen@ecombest.com>
2023-04-16 17:51:01 +0700

    setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+# Metadata goes in pyproject.toml.
+# These are here for GitHub's dependency graph.
+setup(
+    name="nats-client-py",
+    version="0.0.1",
+    extras_require={
+        # 'nkeys': ['nkeys']
+    },
+)
